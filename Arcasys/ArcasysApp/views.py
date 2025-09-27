50a43ba42de7cb440564618a87a29b1c5ec77c96
LikeYasay <lichael456@gmail.com>
2025-09-27 23:42:12 +0800

    Added base.html, login/register/events pages, CSS updates, and modified settings/urls/views

--- Arcasys/ArcasysApp/views.py
+++ Arcasys/ArcasysApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing(request):
+    return render(request, "ArcasysApp/landing.html")
+
+def login_view(request):
+    return render(request, 'ArcasysApp/login.html')
+
+def register_view(request):
+    return render(request, 'ArcasysApp/register.html')
+
+def events_view(request):
+    return render(request, "ArcasysApp/events.html")

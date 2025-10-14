0ad0c939f089b6c93b5305fd4693e734481fdd0b
Emman Jay  Uy <emmanjayuy@gmail.com>
2025-10-15 04:06:56 +0800

    feature(users): complete auth module with images, forms, admin, and updated static paths

---
+++ Arcasys/apps/users/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from ArcasysApp.models import User, Role  # Keep models centralized for now
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('UserEmail', 'UserFullName', 'RoleID', 'isUserStaff', 'UserCreatedAt', 'isUserActive')
+    list_filter = ('RoleID', 'isUserStaff', 'isUserActive', 'UserCreatedAt')
+    fieldsets = (
+        (None, {'fields': ('UserEmail', 'password')}),
+        ('Personal info', {'fields': ('UserFullName', 'RoleID')}),
+        ('Permissions', {'fields': ('isUserActive', 'isUserStaff', 'isUserAdmin')}),
+        ('Important dates', {'fields': ('UserLastLogin', 'UserCreatedAt', 'UserApprovedAt')}),
+        ('Approval Info', {'fields': ('UserApprovedBy',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('UserEmail', 'UserFullName', 'password1', 'password2', 'RoleID', 'isUserStaff', 'isUserActive'),
+        }),
+    )
+    search_fields = ('UserEmail', 'UserFullName')
+    ordering = ('UserCreatedAt',)
+    filter_horizontal = ()
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Role)
